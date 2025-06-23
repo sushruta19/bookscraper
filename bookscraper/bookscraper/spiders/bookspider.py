3cc09107d90f4fb8dfdf5b54426255db3b77e3c9
Soubhik Sen <sensoubhik2001@gmail.com>
2025-06-23 16:25:29 +0530

    Scraped all the pages and displayed in terminal

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -16,3 +16,10 @@ class BookspiderSpider(scrapy.Spider):
                 'rating': book.css('p.star-rating::attr(class)').get().split()[-1],
                 'url': "https://books.toscrape.com/"+book.css('h3 a::attr(href)').get()
             }
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
