17964cdbb4a61e1f98b9cedd8d3c319119285068
Soubhik Sen <sensoubhik2001@gmail.com>
2025-06-23 16:16:59 +0530

    Scraped the first page of bookstoscrape website
    Diplayed the name, price, url, rating, etc of each book in terminal

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,12 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+        for book in books:
+            yield {
+                'name':book.css('a::text').get(),
+                'price':book.css('p.price_color::text').get(),
+                'availability':' '.join(book.css('p.instock::text').getall()).strip(),
+                'rating': book.css('p.star-rating::attr(class)').get().split()[-1],
+                'url': "https://books.toscrape.com/"+book.css('h3 a::attr(href)').get()
+            }
