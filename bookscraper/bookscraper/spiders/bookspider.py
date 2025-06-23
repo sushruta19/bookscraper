e68105f63f83be46962e32c7b7be415e5ec3923e
Soubhik Sen <sensoubhik2001@gmail.com>
2025-06-23 21:06:51 +0530

    Follow book links to scrape detailed info of each book

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -9,13 +9,10 @@ class BookspiderSpider(scrapy.Spider):
     def parse(self, response):
         books = response.css('article.product_pod')
         for book in books:
-            yield {
-                'name':book.css('a::text').get(),
-                'price':book.css('p.price_color::text').get(),
-                'availability':' '.join(book.css('p.instock::text').getall()).strip(),
-                'rating': book.css('p.star-rating::attr(class)').get().split()[-1],
-                'url': "https://books.toscrape.com/"+book.css('h3 a::attr(href)').get()
-            }
+            relative_url = book.css('h3 a::attr(href)').get()
+            book_url = "https://books.toscrape.com/" + relative_url
+            yield response.follow(book_url, callback=self.parse_book_page)
+            
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             if "catalogue/" in next_page:
@@ -23,3 +20,20 @@ class BookspiderSpider(scrapy.Spider):
             else:
                 next_page_url = "https://books.toscrape.com/catalogue/" + next_page
             yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_book_page(self, response):
+        table_rows = response.css('table tr')
+        yield {
+            'url': response.url,
+            'title': response.css('.product_page h1::text').get(),
+            'product_type': table_rows[1].css('td::text').get(),
+            'price_excl_tax': table_rows[2].css('td::text').get(),
+            'price_incl_tax': table_rows[3].css('td::text').get(),
+            'tax' : table_rows[4].css('td::text').get(),
+            'availability': table_rows[5].css('td::text').get(),
+            'num_reviews': table_rows[6].css('td::text').get(),
+            'rating': response.css('p.star-rating::attr(class)').get().split()[-1],
+            'category': response.css('ul.breadcrumb li:nth-child(3) a::text').get(),
+            'description': response.css('div#product_description + p::text').get(),
+            'price': response.css('p.price_color ::text').get()
+        }
