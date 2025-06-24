81a99604502728187ebbc3ae4483146d0bacf7e4
Soubhik Sen <sensoubhik2001@gmail.com>
2025-06-24 21:55:22 +0530

    Add random User-Agent rotation in requests in bookspider

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -1,5 +1,6 @@
 import scrapy
 from bookscraper.items import BookItem
+import random
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
@@ -11,6 +12,13 @@ class BookspiderSpider(scrapy.Spider):
     #         'booksCleanData.json' : {'format': 'json', 'overwrite': True} 
     #     }
     # }
+    user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+    ]
 
     def parse(self, response):
         books = response.css('article.product_pod')
@@ -18,7 +26,11 @@ class BookspiderSpider(scrapy.Spider):
             relative_url = book.css('h3 a::attr(href)').get()
             # book_url = "https://books.toscrape.com/" + relative_url
             book_url = response.urljoin(relative_url)
-            yield response.follow(book_url, callback=self.parse_book_page)
+            yield response.follow(
+                book_url, 
+                callback=self.parse_book_page, 
+                headers={"User-Agent": self.user_agent_list[random.randint(0, len(self.user_agent_list)-1)]}
+            )
             
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
@@ -26,7 +38,11 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = "https://books.toscrape.com/" + next_page
             else:
                 next_page_url = "https://books.toscrape.com/catalogue/" + next_page
-            yield response.follow(next_page_url, callback=self.parse)
+            yield response.follow(
+                next_page_url, 
+                callback=self.parse,
+                headers={"User-Agent": self.user_agent_list[random.randint(0, len(self.user_agent_list)-1)]}   
+            )
 
     def parse_book_page(self, response):
         table_rows = response.css('table tr')
